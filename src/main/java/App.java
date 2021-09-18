43f699ea0162dc140c241d8ea0e7d4d5c511a3eb
andreassenmarvin <marvinandreassen@gmail.com>
2021-09-18 13:04:02 +0300

    Initial commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,9 @@
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.*;
@@ -29,6 +31,36 @@ public class App {
         get("/create/animal",(request, response) -> {
             Map<String,Object> model=new HashMap<String, Object>();
             return new ModelAndView(model,"animals-form.hbs");
+        },new HandlebarsTemplateEngine());
+
+        post("/create/animal/new",(request, response) -> {
+            Map<String,Object> model=new HashMap<String, Object>();
+            String name=request.queryParams("name");
+            System.out.println(name);
+            String type=request.queryParams("type");
+            System.out.println(type);
+            String age=request.queryParams("age");
+            System.out.println(age);
+            String health=request.queryParams("health");
+            System.out.println(health);
+            return new ModelAndView(model,"animal-form.hbs");
+        },new HandlebarsTemplateEngine());
+
+        get("/create/animal/endangered",(request, response) -> {
+            Map<String,Object> model=new HashMap<String, Object>();
+            List<String> health= new ArrayList<String>();
+            health.add(EndangeredAnimals.HEALTH_HEALTHY);
+            health.add(EndangeredAnimals.HEALTH_ILL);
+            health.add(EndangeredAnimals.HEALTH_OKAY);
+            List<String> age= new ArrayList<String>();
+            age.add(EndangeredAnimals.AGE_ADULT);
+            age.add(EndangeredAnimals.AGE_NEWBORN);
+            age.add(EndangeredAnimals.AGE_YOUNG);
+            model.put("health",health);
+            model.put("age",age);
+            String typeChosen="endangered";
+            model.put("endangered",typeChosen);
+            return new ModelAndView(model,"animal-form.hbs");
         },new HandlebarsTemplateEngine());
     }
 }
